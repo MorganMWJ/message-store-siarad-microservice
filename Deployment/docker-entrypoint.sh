255caf50aecd0eea6453bf0a8859530fcc1568a5
Morgan Jones <aburdjones@gmail.com>
2019-12-05 12:41:13 +0000

    Added auto deploy and cut down entrypoint.sh

--- Deployment/docker-entrypoint.sh
+++ Deployment/docker-entrypoint.sh
@@ -6,15 +6,15 @@ echo "##########GENERATED ADMIN PASSWORD: $ADMIN_PASSWORD
 ##########"
 fi
 
-echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
-echo "AS_ADMIN_NEWPASSWORD=${ADMIN_PASSWORD}" >> /tmp/glassfishpwd
+#echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
+#echo "AS_ADMIN_NEWPASSWORD=${ADMIN_PASSWORD}" >> /tmp/glassfishpwd
 
-asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
-asadmin start-domain
+#asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
+#asadmin start-domain
 
-echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}" > /tmp/glassfishpwd
+#echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}" > /tmp/glassfishpwd
 
-asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
+#asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
 
 asadmin create-jdbc-connection-pool --datasourceclassname org.postgresql.ds.PGConnectionPoolDataSource
 			--restype javax.sql.ConnectionPoolDataSource
@@ -27,7 +27,7 @@ asadmin create-jdbc-resource --connectionpoolid message_store_db_pool jdbc/messa
 #--property "jaas-context=ldapRealm:directory=ldap\://ldap.dcs.aber.ac.uk/:base-dn=ou\=People,dc\=dcs,dc\=aber,dc\=ac,dc\=uk:search-filter=uid\=\%s:group-base-dn=ou\=Group,dc\=dcs,dc\=aber,dc\=ac,dc\=uk:group-search-filter=memberUid\=\%s"\
 # dcs_ldap_realm
 
-asadmin --user=admin stop-domain
-rm /tmp/glassfishpwd
+#asadmin --user=admin stop-domain
+#rm /tmp/glassfishpwd
 
 exec "$@"
