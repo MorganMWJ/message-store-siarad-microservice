8151d12b4f0557f7d7e524b84e19fd05e6cd0789
Morgan Jones <mwj7@m56-docker1.dcs.aber.ac.uk>
2019-12-05 12:26:11 +0000

    Alternate working glssfish docker file without my jdbc resource or app deployed

--- Deployment/docker-entrypoint.sh
+++ Deployment/docker-entrypoint.sh
@@ -18,10 +18,10 @@ asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
 
 asadmin create-jdbc-connection-pool --datasourceclassname org.postgresql.ds.PGConnectionPoolDataSource
 			--restype javax.sql.ConnectionPoolDataSource
-			--property User=admin:PortNumber:5100:Password=admin:ServerName=localhost:DatabaseName=message_store
+			--property User=admin:PortNumber:5100:Password=admin:ServerName=m56-docker1.dcs.aber.ac.uk:DatabaseName=message_store
       message_store_db_pool
 
-asadmin create-jdbc-resource --connectionpoolid message_store_db_pool jdbc/message_store
+asadmin create-jdbc-resource --connectionpoolid message_store_db_pool jdbc/messageStoreOffCampusDBRes
 
 #asadmin --user=admin --passwordfile=/tmp/glassfishpwd create-auth-realm --classname com.sun.enterprise.security.auth.realm.ldap.LDAPRealm \
 #--property "jaas-context=ldapRealm:directory=ldap\://ldap.dcs.aber.ac.uk/:base-dn=ou\=People,dc\=dcs,dc\=aber,dc\=ac,dc\=uk:search-filter=uid\=\%s:group-base-dn=ou\=Group,dc\=dcs,dc\=aber,dc\=ac,dc\=uk:group-search-filter=memberUid\=\%s"\
